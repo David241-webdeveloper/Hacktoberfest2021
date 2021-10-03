1ab18324972caec8805bfe7a3604c508b0c961a2
Diya Karmakar <diya20_ug@ece.nits.ac.in>
2021-10-03 18:43:19 +0530

    changes made in the structure and style

---
+++ print.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    //Declaration of char variable
+    char str[100];
+    cout<<"Enter your string :\n";
+    cin.getline(str,100);
+
+    cout<<"Entered string :"<<str;
+    return 0;
+}
