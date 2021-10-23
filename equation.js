a201978d891c2f4898b84b522c93ce5509a6083c
Kizito007 <nwakakelechi49@gmail.com>
2021-10-23 13:28:44 +0100

    Quadratic equation buildup

---
+++ equation.js
@@ -0,0 +1,13 @@
+ //FIGURES
+ var a= 40;
+ var b= 10;
+ var c= 12;
+calculation
+var m= 4 *a *c;
+var sqr= b*b;
+var sub= sqr- m;
+var root= sub ^ 0.5;
+var osc= -b + root;
+var den= 2* a;
+var answer= osc/ den;
+console.log(answer);
